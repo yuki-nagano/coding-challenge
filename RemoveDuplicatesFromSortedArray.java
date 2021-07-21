e5bb1fd56b45d9ab7201d670bf753bd6e65a3eb1
Yuki Nagano <newsnsd.24@gmail.com>
2021-07-21 15:43:35 -0700

    💛 2nd attempt, solved! still can be optimized

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -1,14 +1,40 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // input: [1,1,2]
-        // output: 2 [1,2] number of the unique elements
+        int k = nums.length;
+        int i = 0;
+        int j = 1;
 
-        // loop through nums
-        // if the element exists in nums, remove the array and i--
-        // otherwise, keep going
-        //    i
-        // [1,1,2]
-        // []
+        //
+        // 1M234MM
+        //     i
+        //        j
+        while (j < nums.length){
+            if (nums[i] == nums[j]) {
+                k -= 1;
+                nums[j] = Integer.MAX_VALUE;
+                j++;
+            } else {
+                i = j;
+                j++;
+            }
+        }
+        Arrays.sort(nums); // O(nlogn)
+        nums = new int[k];
+        return k;
+    }
+}
+
+/*
+first attempt
+        input: [1,1,2]
+        output: 2 [1,2] number of the unique elements
+
+        loop through nums
+        if the element exists in nums, remove the array and i--
+        otherwise, keep going
+           i
+        [1,1,2]
+        []
 
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
@@ -18,5 +44,4 @@ class Solution {
                 set.add(nums[i]);
             }
         }
-    }
-}
+*/
