c1711da871332fdf5450b9972488d327a4ececad
Yuki Nagano <newsnsd.24@gmail.com>
2021-07-21 11:55:46 -0700

    🖤 still diagramming

--- MergeTwoSortedList.java
+++ MergeTwoSortedList.java
@@ -10,6 +10,39 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode cur = l1;
+        ListNode pre = l1;
+
+/*                  $
+                    c
+            l1 = [1,2]
+                  |
+            l2 = [2,3]
+                  p
+                    $                  */
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur = cur.next;
+                pre.next = l2; // 1-2-3
+                l1 = l1.next;
+            } else if (l1.val == l2.val) {
+                l2 = l2.next; // 3
+                pre.next = cur; // 1-2-2
+            }
+        }
+        return pre;
+
+        // - l1cur.val = l2cur.val,
+        //     cur2 = cur2.next (cur2: 3-4)
+        //     prev.next = l1 (prev: 1-1-2-4)
+        // - l1 > l2
+
+
+    }
+}
+        // attempt 1 note:
         //   p   c
         // l1 = [1,2,4]
         //       | t
@@ -25,6 +58,3 @@ class Solution {
 
         // else if l1.cur > l2.cur (ex. 3 vs. 2)
         //
-
-    }
-}
