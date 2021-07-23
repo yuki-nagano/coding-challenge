75c8ec8018e99b6a67fdfedf2f2213cee8897cc5
Yuki Nagano <newsnsd.24@gmail.com>
2021-07-22 20:19:49 -0700

    🖤 2nd attempt, didn't pass some tests

--- SameTree.java
+++ SameTree.java
@@ -18,6 +18,51 @@ class Solution {
     public boolean result = false;
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        // pattern1
+        //    1
+        //  2   2
+
+        // pattern2 false
+        //    1                1
+        //  2   null       null  2
+
+        // pattern3 true
+        //    1                1
+        //  2   null       2     null
+
+        //    null              null
+
+        if (p == null && q == null) {
+            result = true;
+        } else {
+            helper(p, q);
+        }
+        return result;
+    }
+
+    private void helper(TreeNode p, TreeNode q) {
+
+        if (p == null || q == null) {
+            return;
+        }
+
+        // either node is null
+        if ((p.left == null && q.left != null) || (p.right == null && q.right != null)) {
+            result = false;
+            return;
+        } else if (p.val != q.val) {
+            result = false;
+        } else if (p.left == q.left && p.right == q.right) {
+            result = true;
+        }
+
+        helper(p.right, q.right);
+        helper(p.left, q.left);
+    }
+}
+
+/* 1st attempt
         // root, right, left, if there was something wrong, false
         return helper(p, q);
     }
@@ -45,5 +90,5 @@ class Solution {
         }
         return result;
     }
-
 }
+*/
