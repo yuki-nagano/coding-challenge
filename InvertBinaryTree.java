1d83101962a03c92e0582c264e8db7245c81d880
Yuki Nagano <newsnsd.24@gmail.com>
2021-08-12 22:38:01 -0700

    🖤 2nd attempt, couldn't finish

--- InvertBinaryTree.java
+++ InvertBinaryTree.java
@@ -14,7 +14,40 @@
  * }
  */
 class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        return helper(root.left, root.right);
+    }
 
+    private TreeNode helper(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return;
+        // swap
+        TreeNode dummyT1 = t1;
+        t1 = t2;
+        t2 = dummyT1;
+
+        // go to the next node
+        t1 = helper(t1.left, t1.right);
+        t2 = helper(t2.left, t2.right);
+    }
+}
+
+/*
+Note:
+    helper(root.left, root.right)
+    helper(TreeNode t1, TreeNode t2)
+        // null check
+        if (t1 == null && t2 == null) return;
+        // swap
+        TreeNode temp = t1;
+        t1 = t2;
+        t2 = temp;
+
+        // go to the next node
+        helper(t1.left, t1.right)
+        helper(t2.left, t2.right)
+*/
+
+/* 1st attempt
     public TreeNode invertedRoot = new TreeNode();
 
     public TreeNode invertTree(TreeNode root) {
@@ -29,17 +62,17 @@ class Solution {
 
     private void invert(TreeNode invertedRoot, TreeNode root) {
         // [2,1,3,2] -> [2,3,1,2]
-        /*      2               2
+             2               2
              /    \           /    \
             1       3       3       1
               2                       2
-        */
+
         if (root == null && invertedRoot == null) {
             return;
         }
-//         if (root.right != null && root.left != null) {
+         if (root.right != null && root.left != null) {
 
-//         }
+         }
 
         invertedRoot.left = root.right;
         invertedRoot.right = root.left;
