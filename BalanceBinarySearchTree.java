0bc91368be17814c0b983508be876b5b7045a9c3
Yuki Nagano <newsnsd.24@gmail.com>
2021-08-25 11:54:21 -0700

    🖤 2nd attempt, still diagramming

--- BalanceBinarySearchTree.java
+++ BalanceBinarySearchTree.java
@@ -1,3 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode balanceBST(TreeNode root) {
+
+    }
+}
+
+/*
+Note:
+    it's balanced
+     - the level diff is less than 1
+
+    how to rotate
+    - store root.val for later
+    - change root to root.left
+    -
+
+    storenode = root.val // 3
+    storetree = root
+    root.val = root.right.val; // 5 either node which is bigger
+
+    method:find the right position of formar root(3)
+    def findtherightpos(root, storednode)
+
+            3
+        /       \
+       1          5
+                    \
+                      6
+                        \
+                          7
+*/
+
 /* First attempt
 Note:
 binary tree
