2057b928728456c107fac99a3684b1821a52ddfc
Yuki Nagano <newsnsd.24@gmail.com>
2021-09-06 12:41:36 -0700

    🖤 couldn't solve

---
+++ ReverseBits.java
@@ -0,0 +1,17 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+
+    }
+}
+/*
+Note:
+    input: 0010 1001 0100 0001 1110 1001 1100
+    1. put every 4 digits into an array/stack
+    2. pop and append
+    3. convert into integer and return it
+
+    Divide and conquer O(nlogn)
+    1. put every 4 digits into an array
+    2. find half way
+*/
