e94d07de24361f18c5aef04a321540128baafe61
Yuki Nagano <newsnsd.24@gmail.com>
2022-03-10 20:49:20 -0800

    🖤 3rd attempt, saw solution

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -1,30 +1,24 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k = nums.length;
         int i = 0;
-        int j = 1;
-
-        //
-        // 1M234MM
-        //     i
-        //        j
-        while (j < nums.length){
-            if (nums[i] == nums[j]) {
-                k -= 1;
-                nums[j] = Integer.MAX_VALUE;
-                j++;
-            } else {
-                i = j;
-                j++;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
             }
         }
-        Arrays.sort(nums); // O(nlogn)
-        nums = new int[k];
-        return k;
+        return i + 1;
     }
 }
 
 /*
+Thrid
+                           i         j
+Input: nums =     [0,1,2,3,1,4,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+if i == j
+    nums = new int[length + 1]
+
 first attempt
         input: [1,1,2]
         output: 2 [1,2] number of the unique elements
@@ -44,4 +38,26 @@ first attempt
                 set.add(nums[i]);
             }
         }
+second attempt
+    int k = nums.length;
+    int i = 0;
+    int j = 1;
+
+    //
+    // 1M234MM
+    //     i
+    //        j
+    while (j < nums.length){
+        if (nums[i] == nums[j]) {
+            k -= 1;
+            nums[j] = Integer.MAX_VALUE;
+            j++;
+        } else {
+            i = j;
+            j++;
+        }
+    }
+    Arrays.sort(nums); // O(nlogn)
+    nums = new int[k];
+    return k;
 */
