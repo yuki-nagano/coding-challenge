665cc3c250bbac8bf2ae4debb7b8ef0d92468fe8
Yuki Nagano <newsnsd.24@gmail.com>
2023-10-12 22:37:45 -0700

    💖️

--- python/ConcatenationOfArray.py
+++ python/ConcatenationOfArray.py
@@ -13,3 +13,11 @@ class Solution:
 nums = [1,2,1]
 ans = [1,2,1,1,2,1]
 '''
+
+# 2nd time
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        copy = nums.copy()
+        for n in copy:
+            nums.append(n)
+        return nums
