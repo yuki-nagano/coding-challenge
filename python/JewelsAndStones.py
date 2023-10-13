c53b12099d8a6b6b2079b87478b5e99c959793fe
Yuki Nagano <32025126+yuki-nagano@users.noreply.github.com>
2023-10-13 17:07:27 -0400

    🖤

--- python/JewelsAndStones.py
+++ python/JewelsAndStones.py
@@ -13,3 +13,22 @@ class Solution:
     loop through stones, check if the stone contains
     count+= 1
 '''
+
+# 2nd time - didn't consider set
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewels_list: dict = {}
+        for j in range(len(jewels)):
+            jewels_list = {jewels[j]: 0}
+
+        for s in range(len(stones)):
+            if jewels_list.get(stones[s]):
+                jewels_list[s] = jewels_list.get(s) + 1
+
+        return sum(jewels_list.values())
+
+"""
+    aA aAAbbbb
+    a: 1
+    A: 2
+"""
